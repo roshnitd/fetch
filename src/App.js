87be5fe92e56264a2ca9468fa4e06f671f87e281
roshnitd <roshnisdt512@gmail.com>
2023-06-21 12:36:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [posts, setPosts]=useState([])
+
+
+  useEffect(() =>{
+  fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => setPosts(posts))
+  },[])
+
+
+  return  <div className="App">
+    <h1>Fetch API Data</h1>
+    {posts.map((item)=>{
+return <h1 key={item.id}>{item.title}</h1>
+    })}
+    </div>;
+  
 }
 
 export default App;
